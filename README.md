2239ff15aed98175b9a10b090e50de1b3640e0fc
ernesthenry <henry38ernest@gmail.com>
2019-10-11 09:53:22 -0400

    create reusable components using props

---
+++ README.md
@@ -0,0 +1,1 @@
+### This tutorial covers the basics of React
--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-const Header = () => {
+const Header = (props) => {
+    console.log(props)
     return (
         <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
+            <h1>{props.title}</h1>
+            <span className="stats">Players: {props.totalPlayers}</span>
         </header>
 
     );
 }
 
 
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
-                Marino
+                {props.name}
             </span>
-            <Counter />
+            <Counter score={props.score}/>
         </div>
     )
 }
 
-const Counter = () => {
+const Counter = (props) => {
     return (
         <div className="counter">
             <button className="counter-action decrement"> - </button>
-            <span className="counter-score">35</span>
+            <span className="counter-score">{props.score}</span>
             <button className="counter-action increment"> + </button>
         </div>
     );
@@ -33,10 +34,18 @@ const Counter = () => {
 const App = () => {
     return(
         <div className="scoreboard">
-        <Header />
+        <Header 
+        title="Scoreboard"
+        totalPlayers={1}
+        />
     
         {/* Players list */}
-        <Player />
+        <Player name="Marino" score={50}/>
+        <Player name="Ernest" score={40}/>
+        <Player name="Waswa" score={30}/>
+        <Player name="Kato" score={11}/>
+        <Player name="Henry" score={18}/>
+
         </div>
         
        
